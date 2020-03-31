254efc3c46af751b18703129099c8f77a13ab359
dongbin <rewgh0@github.com>
2020-03-31 23:45:25 +0900

    Initial commit

---
+++ app/src/main/java/com/example/settinglibrary/Msg.java
@@ -0,0 +1,7 @@
+package com.example.settinglibrary;
+
+public class Msg {
+    protected static final String FRAGMENT_TAG = "Camera";
+    protected static final int REQUEST_CAMERA_PERMISSIONS = 931;
+    protected static final int REQUEST_PREVIEW_CODE = 1001;
+}
